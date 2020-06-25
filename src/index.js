d36299865803fa8ed2fff0345b6c32c0b8f93cf7
Joshua Ji <joshuanianji@users.noreply.github.com>
2020-06-24 21:41:57 -0600

    JS Changes: - import sounds so create-elm-app recognizes them - make all other sounds quieter so ticks are louder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './assets/howler.core';
 import './assets/simptip.min.css';
 
 // images
-import smh from "./assets/img/smh.png"
+import smh from './assets/img/smh.png'
+import tadamp3 from './assets/sounds/tada.mp3'
+import tadawav from './assets/sounds/tada.wav'
+import tickmp3 from './assets/sounds/tick.mp3'
+import whistlemp3 from './assets/sounds/whistle.mp3'
 
 // other imports
 import { Elm } from './Main.elm';
@@ -14,15 +18,18 @@ import * as serviceWorker from './serviceWorker';
 // init sounds
 
 const whistle = new Howl({
-    src: ['./assets/sounds/whistle.mp3']
+    volume: 0.8,
+    src: [whistlemp3]
 });
 
 const tada = new Howl({
-    src: ['./assets/sounds/tada.mp3', './assets/sounds/tada.wav']
+    volume: 0.8,
+    src: [tadamp3, tadawav]
 });
 
 const tick = new Howl({
-    src: ['./assets/sounds/tick.mp3']
+    volume: 1,
+    src: [tickmp3]
 });
 
 var storedConfig = localStorage.getItem('config');
